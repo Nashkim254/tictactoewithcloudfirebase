ca81a2f2bcb4fe97b1acbc6d700bd15da8495308
programming6131 <kimutainashon@gmail.com>
2022-04-12 21:35:57 +0300

    google sign in

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:tic_tac_toe/rounded_btn/rounded_btn.dart';
 import 'package:tic_tac_toe/screens/create_account/create_account.dart';
@@ -18,9 +19,26 @@ class _LoginState extends State<Login> {
   bool showSpinner = false;
   final _auth = FirebaseAuth.instance;
   TextEditingController emailCont = TextEditingController();
-TextEditingController passCont = TextEditingController();
+  TextEditingController passCont = TextEditingController();
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-final _formKey = GlobalKey<FormState>();
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return ModalProgressHUD(
@@ -38,29 +56,28 @@ final _formKey = GlobalKey<FormState>();
                 children: [
                   Center(
                     child: SizedBox(
-                      width: 175,
-                      height: 175,
-                      child: SvgPicture.asset('assets/images/login.svg')
-                    ),
+                        width: 175,
+                        height: 175,
+                        child: SvgPicture.asset('assets/images/login.svg')),
                   ),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(20, 15, 20, 8),
-                    child: Text('Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20
-                    ),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text('Please sign in to continue.',
-                        style: TextStyle(
-                        color: Colors.grey[600],
-                        fontWeight: FontWeight.w400,
-                        fontSize: 13
-                    ),
+                    child: Text(
+                      'Please sign in to continue.',
+                      style: TextStyle(
+                          color: Colors.grey[600],
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13),
                     ),
                   ),
                   Padding(
@@ -72,31 +89,37 @@ final _formKey = GlobalKey<FormState>();
                         children: <Widget>[
                           Text(
                             'E-mail',
-                            style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 13,
+                                color: Colors.white),
                           ),
                           SizedBox(
                             height: 10,
                           ),
                           TextFormField(
                             controller: emailCont,
-                            validator: (value)=> Validator().emailValidator(value!),
+                            validator: (value) =>
+                                Validator().emailValidator(value!),
                             style: (TextStyle(
-                             color: Colors.white,
-                             fontWeight: FontWeight.w400
-                            )),
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400)),
                             keyboardType: TextInputType.emailAddress,
-                              cursorColor: Colors.white,
-                              obscureText: false,
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  fillColor: Color(0xfff3B324E),
-                                  filled: true,
-                              prefixIcon: Image.asset('assets/images/icon_email.png'),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                                ),
-                          ),
+                            cursorColor: Colors.white,
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              fillColor: Color(0xfff3B324E),
+                              filled: true,
+                              prefixIcon:
+                                  Image.asset('assets/images/icon_email.png'),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xff14DAE2), width: 2.0),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0)),
+                              ),
+                            ),
                           ),
                         ],
                       ),
@@ -109,28 +132,34 @@ final _formKey = GlobalKey<FormState>();
                       children: <Widget>[
                         Text(
                           'Password',
-                          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 13,
+                              color: Colors.white),
                         ),
                         SizedBox(
                           height: 10,
                         ),
                         TextFormField(
                           controller: passCont,
-                          validator: (value)=> Validator().passwordValidator(value!),
+                          validator: (value) =>
+                              Validator().passwordValidator(value!),
                           style: (TextStyle(
                               color: Colors.white,
-                              fontWeight: FontWeight.w400
-                          )),
+                              fontWeight: FontWeight.w400)),
                           obscureText: true,
                           cursorColor: Colors.white,
                           decoration: InputDecoration(
                             border: InputBorder.none,
                             fillColor: Color(0xfff3B324E),
                             filled: true,
-                            prefixIcon: Image.asset('assets/images/icon_lock.png'),
+                            prefixIcon:
+                                Image.asset('assets/images/icon_lock.png'),
                             focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                              borderSide: BorderSide(
+                                  color: Color(0xff14DAE2), width: 2.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20.0)),
                             ),
                           ),
                         ),
@@ -145,34 +174,44 @@ final _formKey = GlobalKey<FormState>();
                         color: Color(0xff14DAE2),
                         onPressed: () async {
                           // Add login code
-                          if(_formKey.currentState!.validate()){
-                          setState(() {
-                            showSpinner = true;
-                          });
-                          try {
-                            final user = await _auth.signInWithEmailAndPassword(
-                                email: emailCont.text, password: passCont.text);
-                            if (user != null) {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => HomeScreen()));
-                            }
+                          if (_formKey.currentState!.validate()) {
                             setState(() {
-                              showSpinner = false;
+                              showSpinner = true;
                             });
-                          } catch (e) {
-                            print(e);
-                          }}else{}
+                            try {
+                              final user =
+                                  await _auth.signInWithEmailAndPassword(
+                                      email: emailCont.text,
+                                      password: passCont.text);
+                              if (user != null) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomeScreen()));
+                              }
+                              setState(() {
+                                showSpinner = false;
+                              });
+                            } catch (e) {
+                              print(e);
+                            }
+                          } else {}
                         },
                       ),
                     ),
                   ),
-                  Center(
-                    child: Text('Forgot Password?',
-                    style: TextStyle(
-                      color: Color(0xff14DAE2)
-                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: RoundedButton(
+                        btnText: 'GOOGLE SIGN IN',
+                        color: Color(0xff14DAE2),
+                        onPressed: () async {
+                         
+                          await  signInWithGoogle();
+                          
+                        },
+                      ),
                     ),
                   ),
                   SizedBox(
@@ -181,11 +220,12 @@ final _formKey = GlobalKey<FormState>();
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Dont have an account?',
+                      Text(
+                        'Dont have an account?',
                         style: TextStyle(
                             color: Colors.grey[600],
-                            fontWeight: FontWeight.w400
-                        ),),
+                            fontWeight: FontWeight.w400),
+                      ),
                       FlatButton(
                         onPressed: () {
                           Navigator.push(
@@ -194,9 +234,9 @@ final _formKey = GlobalKey<FormState>();
                                   builder: (context) => CreateAccount()));
                         },
                         child: Text('Sign up',
-                          style: TextStyle(
-                              color: Color(0xff14DAE2),)
-                        ),
+                            style: TextStyle(
+                              color: Color(0xff14DAE2),
+                            )),
                       )
                     ],
                   )
